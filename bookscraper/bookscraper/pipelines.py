efe20cb98d873ee34bba27050ea7ffed19d1eb5b
Roman Dietenmeier <roman@d-robotic.de>
2024-03-16 21:31:09 +0100

    Add pipeline to project to mutate the data before it is saved. Enable the pipeline in settings.py. Recreate data.csv/json finish part 6.

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,13 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if adapter.get(field_name) is None:
+                adapter[field_name] = "N/A"
+            adapter[field_name] = adapter[field_name].strip()
+        adapter["url"] = adapter["url"].lower()
         return item
